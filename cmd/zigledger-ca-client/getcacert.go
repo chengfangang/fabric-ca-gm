982f3c1cbf50f57687dbac107e13c55bd1c23221
chengfangang <chengfangang@ziggurat.cn>
2019-01-03 11:27:24 +0800

    change getcacert.go file

--- cmd/zigledger-ca-client/getcacert.go
+++ cmd/zigledger-ca-client/getcacert.go
@@ -33,6 +33,7 @@ import (
 	"github.com/chengfangang/fabric-ca-gm/api"
 	"github.com/chengfangang/fabric-ca-gm/lib"
 	"github.com/chengfangang/fabric-ca-gm/util"
+	"github.com/tjfoc/gmsm/sm2"
 	"github.com/spf13/cobra"
 )
 
@@ -120,9 +121,19 @@ func storeCAChain(config *lib.ClientConfig, si *lib.GetServerInfoResponse) error
 		if block == nil {
 			break
 		}
-
-		cert, err := x509.ParseCertificate(block.Bytes)
+		
+		var cert *x509.Certificate
+		var err error
+		if lib.IsGMConfig() {
+			sm2x509Cert, err := sm2.ParseCertificate(block.Bytes)
+			if err == nil {
+				cert = ParseSm2Certificate2X509(sm2x509Cert)
+			}
+		} else {
+			cert, err = x509.ParseCertificate(block.Bytes)
+		}
 		if err != nil {
+			log.Infof("++++++err:%s++++++\n", err)
 			return errors.Wrap(err, "Failed to parse certificate in the CA chain")
 		}
 
